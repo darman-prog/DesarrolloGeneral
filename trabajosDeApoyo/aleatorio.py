a29de25ab4a5913a4c7bd2f7eaa4e8176bca8144
darman-prog <149440307+darman-prog@users.noreply.github.com>
2025-02-07 19:50:31 -0500

    aleatorio.py

---
+++ trabajosDeApoyo/aleatorio.py
@@ -0,0 +1,73 @@
+import math
+#listado de elementos
+
+"""""ItemsSupermercado = {
+
+    "Bebidas":" Coca Cola ",
+    "Bebidas":" Pepsi ",
+    "Bebidas":" Limonada 2L ",
+    "Bebidas": " Leche 1L ",
+    "Frutas": " zanahoria "
+
+
+
+
+}"""""
+
+
+# x = 5
+# x = str(5)
+
+# x = "5"
+# x = int(x)
+
+# x = "2.5"
+# x = float(x)
+
+# x = "True"
+# x = bool(x)
+
+# print("el valor de pi es = ",math.pi)
+# bucle = True
+# detener = False
+# while detener == False:
+#     NumerosImpares = []
+#     NumerosPares = []
+#     for i in range(100):
+#         print(i)         
+#         if i % 2 == 0:
+#             print("impar")
+#             NumerosPares.append(i)
+#         elif i % 2 == 1:
+#             print(" par ")
+#             NumerosImpares.append(i)
+#         if i == 99:
+#             detener = True 
+# print(f" numeros impares = {NumerosImpares}")
+# print(f"numeros pares = {NumerosPares}")
+# en java para hacer sumas con variables se usaria n = n+1 o n++ o n+=n
+
+
+
+# detener = False
+# contador = 0
+# suma = 0
+
+# while detener == False:
+#     valor = float(input(" Escribe un numero mayor que cero: "))
+#     if valor <= 0:
+#         detener = True
+#     else:
+#         contador += 1
+#         suma += valor
+# else:
+#     print(" holaaaaaaaaaa ")
+
+
+# print(f"Numeros ingresados: {contador}")
+# print(f"Suma: {suma}")
+
+
+# for _ in range(10,-1,-1):
+#     print(_)
+
